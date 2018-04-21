d9bd568ecbb98e117693a7c0b4078096db9f5da0
KristalDuran <31260047+KristalDuran@users.noreply.github.com>
2018-04-21 17:54:48 -0600

    Actualizacion

--- PrograDrones/src/Controller/Main.java
+++ PrograDrones/src/Controller/Main.java
@@ -16,7 +16,6 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args){
         
-        
         Menu menu = new Menu();
         
         GraphMethods graphMethods = new GraphMethods();
